5ecc58908246084216dd23a2e91c3b7494104e17
JuanOrtega2 <202208018@alu.comillas.edu>
2023-03-23 07:50:48 +0100

    Cambios en ejercicios

--- Exercise3.py
+++ Exercise3.py
@@ -13,7 +13,7 @@ import happybase
 import csv
 
 def getConn():
-    return happybase.Connection(host='localhost')
+    return happybase.Connection(host='0.0.0.0')
 connection=getConn()
 #Si al conectar da el error -> thriftpy2.transport.base.TTransportException: TTransportException(type=1, message="Could not connect to ('0.0.0.0', 9090)")
 #ejecutar en una terminal: hbase thrift start -p 9090
@@ -30,7 +30,7 @@ def to_bytes(string):
 column_families = {
     'datos': dict()
 }
-connection.create_table(table_name, column_families)
+#connection.create_table(table_name, column_families)
 print(f'Tabla {table_name} creada en el namespace mbd10_30')
 # Ruta del archivo CSV
 csv_file = '/tmp/nosql/airData/2007.csv'
@@ -45,43 +45,44 @@ with open(csv_file, 'r') as csvfile:
     # Para cada línea en el archivo CSV, insertar un registro en la tabla HBase
     with connection.table(table_name).batch(batch_size=100) as table:
         for row in csvreader:
-            # La primera columna "Year" se usará como la clave de fila en HBase
-            row_key = to_bytes(row[16]+'-'+row[17]+'_'+row[0]+row[1].zfill(2)+row[2].zfill(2)+row[4])
+            
+            row_key = to_bytes(row[16]+'-'+row[17])#+'_'+row[0]+row[1].zfill(2)+row[2].zfill(2)+row[4]
 
             data = {
-                #'info:Year': to_bytes(row[0]),
-                #'info:Month': to_bytes(row[1]),
-                #'info:DayofMonth': to_bytes(row[2]),
-                #'info:DayOfWeek': to_bytes(row[3]),
-                #'info:DepTime': to_bytes(row[4]),
-                #'info:CRSDepTime': to_bytes(row[5]),
-                #'info:ArrTime': to_bytes(row[6]),
-                #'info:CRSArrTime': to_bytes(row[7]),
-                'info:NombreAerolinea': to_bytes(row[8]),#UniqueCarrier
-                'info:FlightNum': to_bytes(row[9]),
-                'info:TailNum': to_bytes(row[10]),
-                'info:Duracion': to_bytes(row[11]),#ActualElapsedTime 
-                #'info:CRSElapsedTime': to_bytes(row[12]),
-                #'info:AirTime': to_bytes(row[13]),
-                'info:LlegRetraso': to_bytes(row[14]),#ArrDelay
-                'info:SalRetraso': to_bytes(row[15]),#DepDelay
-                'info:Origin': to_bytes(row[16]),
-                'info:Dest': to_bytes(row[17]),
-                'info:Distance': to_bytes(row[18]),
-                #'info:TaxiIn': to_bytes(row[19]),
-                #'info:TaxiOut': to_bytes(row[20]),
-                'info:Cancelled': to_bytes(row[21])
-                #'info:CancellationCode': to_bytes(row[22]),
-                #'info:Diverted': to_bytes(row[23]),
-                #'info:CarrierDelay': to_bytes(row[24]),
-                #'info:WeatherDelay': to_bytes(row[25]),
-                #'info:NASDelay': to_bytes(row[26]),
-                #'info:SecurityDelay': to_bytes(row[27]),
-                #'info:LateAircraftDelay': to_bytes(row[28]),
+                #'datos:Year': to_bytes(row[0]),
+                #'datos:Month': to_bytes(row[1]),
+                #'datos:DayofMonth': to_bytes(row[2]),
+                #'datos:DayOfWeek': to_bytes(row[3]),
+                #'datos:DepTime': to_bytes(row[4]),
+                #'datos:CRSDepTime': to_bytes(row[5]),
+                #'datos:ArrTime': to_bytes(row[6]),
+                #'datos:CRSArrTime': to_bytes(row[7]),
+                'datos:NombreAerolinea': to_bytes(row[8]),#UniqueCarrier
+                'datos:FlightNum': to_bytes(row[9]),
+                'datos:TailNum': to_bytes(row[10]),
+                'datos:Duracion': to_bytes(row[11]),#ActualElapsedTime 
+                #'datos:CRSElapsedTime': to_bytes(row[12]),
+                #'datos:AirTime': to_bytes(row[13]),
+                'datos:LlegRetraso': to_bytes(row[14]),#ArrDelay
+                'datos:SalRetraso': to_bytes(row[15]),#DepDelay
+                'datos:Origin': to_bytes(row[16]),
+                'datos:Dest': to_bytes(row[17]),
+                'datos:Distance': to_bytes(row[18]),
+                #'datos:TaxiIn': to_bytes(row[19]),
+                #'datos:TaxiOut': to_bytes(row[20]),
+                'datos:Cancelled': to_bytes(row[21])
+                #'datos:CancellationCode': to_bytes(row[22]),
+                #'datos:Diverted': to_bytes(row[23]),
+                #'datos:CarrierDelay': to_bytes(row[24]),
+                #'datos:WeatherDelay': to_bytes(row[25]),
+                #'datos:NASDelay': to_bytes(row[26]),
+                #'datos:SecurityDelay': to_bytes(row[27]),
+                #'datos:LateAircraftDelay': to_bytes(row[28]),
             }
 
             # Insertar el registro en la tabla HBase
             table.put(row_key, data)
+
 csv_file = '/tmp/nosql/airData/2008.csv'
 
 # Abrir el archivo CSV y leerlo con csv.reader
@@ -98,35 +99,35 @@ with open(csv_file, 'r') as csvfile:
             row_key = to_bytes(row[16]+'-'+row[17]+'_'+row[0]+row[1].zfill(2)+row[2].zfill(2)+row[4])
 
             data = {
-                #'info:Year': to_bytes(row[0]),
-                #'info:Month': to_bytes(row[1]),
-                #'info:DayofMonth': to_bytes(row[2]),
-                #'info:DayOfWeek': to_bytes(row[3]),
-                #'info:DepTime': to_bytes(row[4]),
-                #'info:CRSDepTime': to_bytes(row[5]),
-                #'info:ArrTime': to_bytes(row[6]),
-                #'info:CRSArrTime': to_bytes(row[7]),
-                'info:NombreAerolinea': to_bytes(row[8]),#UniqueCarrier
-                'info:FlightNum': to_bytes(row[9]),
-                'info:TailNum': to_bytes(row[10]),
-                'info:Duracion': to_bytes(row[11]),#ActualElapsedTime 
-                #'info:CRSElapsedTime': to_bytes(row[12]),
-                #'info:AirTime': to_bytes(row[13]),
-                'info:LlegRetraso': to_bytes(row[14]),#ArrDelay
-                'info:SalRetraso': to_bytes(row[15]),#DepDelay
-                'info:Origin': to_bytes(row[16]),
-                'info:Dest': to_bytes(row[17]),
-                'info:Distance': to_bytes(row[18]),
-                #'info:TaxiIn': to_bytes(row[19]),
-                #'info:TaxiOut': to_bytes(row[20]),
-                'info:Cancelled': to_bytes(row[21])
-                #'info:CancellationCode': to_bytes(row[22]),
-                #'info:Diverted': to_bytes(row[23]),
-                #'info:CarrierDelay': to_bytes(row[24]),
-                #'info:WeatherDelay': to_bytes(row[25]),
-                #'info:NASDelay': to_bytes(row[26]),
-                #'info:SecurityDelay': to_bytes(row[27]),
-                #'info:LateAircraftDelay': to_bytes(row[28]),
+                #'datos:Year': to_bytes(row[0]),
+                #'datos:Month': to_bytes(row[1]),
+                #'datos:DayofMonth': to_bytes(row[2]),
+                #'datos:DayOfWeek': to_bytes(row[3]),
+                #'datos:DepTime': to_bytes(row[4]),
+                #'datos:CRSDepTime': to_bytes(row[5]),
+                #'datos:ArrTime': to_bytes(row[6]),
+                #'datos:CRSArrTime': to_bytes(row[7]),
+                'datos:NombreAerolinea': to_bytes(row[8]),#UniqueCarrier
+                'datos:FlightNum': to_bytes(row[9]),
+                'datos:TailNum': to_bytes(row[10]),
+                'datos:Duracion': to_bytes(row[11]),#ActualElapsedTime 
+                #'datos:CRSElapsedTime': to_bytes(row[12]),
+                #'datos:AirTime': to_bytes(row[13]),
+                'datos:LlegRetraso': to_bytes(row[14]),#ArrDelay
+                'datos:SalRetraso': to_bytes(row[15]),#DepDelay
+                'datos:Origin': to_bytes(row[16]),
+                'datos:Dest': to_bytes(row[17]),
+                'datos:Distance': to_bytes(row[18]),
+                #'datos:TaxiIn': to_bytes(row[19]),
+                #'datos:TaxiOut': to_bytes(row[20]),
+                'datos:Cancelled': to_bytes(row[21])
+                #'datos:CancellationCode': to_bytes(row[22]),
+                #'datos:Diverted': to_bytes(row[23]),
+                #'datos:CarrierDelay': to_bytes(row[24]),
+                #'datos:WeatherDelay': to_bytes(row[25]),
+                #'datos:NASDelay': to_bytes(row[26]),
+                #'datos:SecurityDelay': to_bytes(row[27]),
+                #'datos:LateAircraftDelay': to_bytes(row[28]),
             }
 
             # Insertar el registro en la tabla HBase
